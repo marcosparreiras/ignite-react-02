67f25383c70fda88d06908dcdbfca9683bcd96e6
Marcos Parreiras <marcosalp1998@gmail.com>
2024-07-31 23:55:50 -0300

    feat: add react-router-dom to manage pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { History } from "./pages/History";
+import { Home } from "./pages/Home";
 
 export function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/history", element: <History /> },
+  ]);
+
   return (
     <ThemeProvider theme={defaultTheme}>
-      Hello World
+      <RouterProvider router={router} />
       <GlobalStyle />
     </ThemeProvider>
   );
